b6b1d264c8885cf5342b9d24c2f175ab4776e4ac
Adam Saponara <as@php.net>
2019-09-11 23:32:14 -0400

    some opl2 sounds

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
 
-    lv2h_new(44100, 128, 1, &host);
+    lv2h_new(44100, 128, 10, &host);
 
     // lv2h_plug_new(host, "http://drobilla.net/plugins/mda/JX10", &plug[0]);
     // lv2h_inst_new(plug[0], &inst[0]);
@@ -32,8 +32,9 @@ int main(int argc, char **argv) {
     // lv2h_inst_set_param(inst[0], "adsr_r", 0.0);
     // lv2h_inst_set_param(inst[0], "adsr_d", 0.0);
 
-    lv2h_plug_new(host, "git://github.com/Themaister/fmsynth", &plug[0]);
+    lv2h_plug_new(host, "https://github.com/jpcima/ADLplug", &plug[0]);
     lv2h_inst_new(plug[0], &inst[0]);
+    lv2h_inst_set_param(inst[0], "volume", 0.05);
 
     lv2h_plug_new(host, "http://calf.sourceforge.net/plugins/VintageDelay", &plug[1]);
     lv2h_inst_new(plug[1], &inst[1]);
@@ -43,13 +44,14 @@ int main(int argc, char **argv) {
     lv2h_inst_set_param(inst[2], "adsr_r", 0.0);
     lv2h_inst_set_param(inst[2], "adsr_d", 0.0);
     lv2h_inst_set_param(inst[2], "o1_wave", 1.0);
+    lv2h_inst_set_param(inst[2], "master", 5.0);
 
     //lv2h_inst_connect(inst[0], "left_out", inst[1], "in_l");
     //lv2h_inst_connect(inst[0], "right_out", inst[1], "in_l");
     //lv2h_inst_connect(inst[0], "out_l", inst[1], "in_l");
     //lv2h_inst_connect(inst[0], "out_r", inst[1], "in_l");
-    lv2h_inst_connect(inst[0], "output_left", inst[1], "in_l");
-    lv2h_inst_connect(inst[0], "output_right", inst[1], "in_l");
+    lv2h_inst_connect(inst[0], "lv2_audio_out_1", inst[1], "in_l");
+    lv2h_inst_connect(inst[0], "lv2_audio_out_2", inst[1], "in_r");
 
     lv2h_inst_connect_to_audio(inst[1], "out_l", 0);
     lv2h_inst_connect_to_audio(inst[1], "out_r", 1);
@@ -58,9 +60,9 @@ int main(int argc, char **argv) {
     lv2h_inst_connect_to_audio(inst[2], "out_r", 1);
 
     lv2h_node_new(host, lv2h_node_callback0, NULL, &node[0]);
-    lv2h_node_set_interval(node[0], 1000);
-    lv2h_node_set_interval_factor(node[0], 0.80);
-    lv2h_node_set_count_limit(node[0], 24);
+    lv2h_node_set_interval(node[0], 20);
+    lv2h_node_set_interval_factor(node[0], 1.01);
+    //lv2h_node_set_count_limit(node[0], 96);
 
     lv2h_node_new(host, lv2h_node_callback1, NULL, &node[1]);
     lv2h_node_set_divisor(node[1], 3);
@@ -69,7 +71,7 @@ int main(int argc, char **argv) {
     // pthread_create(&play_thread, NULL, lv2h_run_play, host);
     pthread_create(&audio_thread, NULL, lv2h_run_audio, host);
 
-    sleep(1);
+    //sleep(1);
 
     lv2h_run(host);
     lv2h_free(host);
@@ -82,17 +84,40 @@ int main(int argc, char **argv) {
 
 static int lv2h_node_callback0(lv2h_node_t *node, void *udata, int count) {
     //printf("0count=%d\n", count);
-    uint8_t note;
-    note = (count + 0x45) % 0x7f;
-    lv2h_inst_play(inst[0], "midi", 0, note, -1, -1, -1, 0x70, 300);
+    uint8_t note, reg, val;
+
+    note = (count*7 + 0x30) % 0x7f;
+
+    lv2h_inst_play(inst[0], "lv2_events_in", 0x00, note, -1, -1, -1, 0x70, 300);
+
+    lv2h_inst_play(inst[0], "lv2_events_in", 0x09, note, -1, -1, -1, 0x70, 300);
+
     return 0;
 }
 
 static int lv2h_node_callback1(lv2h_node_t *node, void *udata, int count) {
     //printf("1count=%d\n", count);
-    uint8_t note;
-    note = (count + 0x30) % 0x7f;
-    lv2h_inst_play(inst[2], "midi_in", 0, note, -1, -1, -1, 0x70, 300);
+    uint8_t note, reg, val;
+    uint8_t sysex[8] = { 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf7 };
+
+    note = (count*11 + 0x30) % 0x7f;
+
+    reg = 0xA0;
+    val = note % 0xFF;
+    sysex[3] = reg >> 7;
+    sysex[4] = reg & 0x7F;
+    sysex[5] = val >> 7;
+    sysex[6] = val & 0x7F;
+    lv2h_inst_send_midi(inst[0], "lv2_events_in", sysex, 8);
+
+    reg = 0xB0;
+    val = 0x20 + (count % 0x1F);
+    sysex[3] = reg >> 7;
+    sysex[4] = reg & 0x7F;
+    sysex[5] = val >> 7;
+    sysex[6] = val & 0x7F;
+    lv2h_inst_send_midi(inst[0], "lv2_events_in", sysex, 8);
+
     return 0;
 }
 
